48d6db9c1262d0b9ec1cbd83d3d25109984d1e24
Diksha Trivedi <diksha.trivedi2122@gmail.com>
2025-03-03 11:26:49 +0530

    Used Service Layer to get message

--- src/main/java/com/example/greeting_app/controller/GreetingController.java
+++ src/main/java/com/example/greeting_app/controller/GreetingController.java
@@ -1,28 +1,23 @@
 package com.example.greeting_app.controller;
 
+import com.example.greeting_app.service.GreetingService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    private final GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService){
+        this.greetingService=greetingService;
+    }
+
+
     @GetMapping
     public String getGreeting() {
-        return "{\"message\": \"Hello, GET request received!\"}";
+        return "{\"message\": \"" + greetingService.getGreetingMessage() + "\"}";
     }
 
-    @PostMapping
-    public String postGreeting() {
-        return "{\"message\": \"Hello, POST request received!\"}";
-    }
 
-    @PutMapping
-    public String putGreeting() {
-        return "{\"message\": \"Hello, PUT request received!\"}";
-    }
-
-    @DeleteMapping
-    public String deleteGreeting() {
-        return "{\"message\": \"Hello, DELETE request received!\"}";
-    }
 }
